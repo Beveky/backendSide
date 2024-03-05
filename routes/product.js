fcbb314b58654f7277168937078f3dc93ee11e26
Beveky <im.beveky@gmail.com>
2024-03-05 22:42:44 +0100

    secondcommit

--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,4 @@
+const cloudinary = require("cloudinary").v2;
 const Product = require("../models/Product");
 const {
   verifyToken,
@@ -6,19 +7,54 @@ const {
 } = require("./verifyToken");
 
 const router = require("express").Router();
+const multer = require("multer");
+const storage = multer.memoryStorage(); // Use memory storage
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Not an image! Please upload only images."), false);
+    }
+  },
+});
 
 //CREATE
 
-router.post("/", verifyTokenAndAdmin, async (req, res) => {
-  const newProduct = new Product(req.body);
+router.post(
+  "/",
+  verifyTokenAndAdmin,
+  upload.single("image"),
+  async (req, res) => {
+    if (!req.file)
+      return res.status(400).json({ message: "No image file provided." });
 
-  try {
-    const savedProduct = await newProduct.save();
-    res.status(200).json(savedProduct);
-  } catch (err) {
-    res.status(500).json(err);
+    try {
+      const result = await new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+          { resource_type: "auto" },
+          (error, result) => {
+            if (error) reject(error);
+            else resolve(result);
+          }
+        );
+        uploadStream.end(req.file.buffer);
+      });
+
+      const newProduct = new Product({
+        ...req.body,
+        img: result.secure_url,
+      });
+
+      const savedProduct = await newProduct.save();
+      res.status(200).json(savedProduct);
+    } catch (err) {
+      res.status(500).json(err.message || err);
+    }
   }
-});
+);
 
 //UPDATE
 router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
